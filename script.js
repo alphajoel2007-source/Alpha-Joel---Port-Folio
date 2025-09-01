43db4d9e7461c75ff05c4bfd2e9f97c9848c0d1f
alphajoel2007-source <alphajoel2007@gmail.com>
2025-09-01 19:56:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Button alert for contact
+document.getElementById("contactBtn").addEventListener("click", function() {
+  alert("Thanks for reaching out! You can contact me at: alpha.joel@example.com");
+});
